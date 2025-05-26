fc0a33717ff8cffe9653e6552136b8deb5e7fff1
Gabriel Dias <gabriel050monteiro@gmail.com>
2025-05-26 01:16:44 -0300

    feat: add Produto repository interface

---
+++ src/main/java/com/empresa/projeto/domain/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.empresa.projeto.domain.repository;
+
+import com.empresa.projeto.domain.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
