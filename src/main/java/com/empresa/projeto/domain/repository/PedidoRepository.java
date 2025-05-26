c5db61ddf9ed03681ea63dbeab06e1a460b0eb5c
Gabriel Dias <gabriel050monteiro@gmail.com>
2025-05-26 05:34:23 -0300

    feat: add PedidoRepository interface
    - Extends JpaRepository for basic CRUD operations
    - Prepare for order query methods

---
+++ src/main/java/com/empresa/projeto/domain/repository/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.empresa.projeto.domain.repository;
+
+import com.empresa.projeto.domain.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+}
